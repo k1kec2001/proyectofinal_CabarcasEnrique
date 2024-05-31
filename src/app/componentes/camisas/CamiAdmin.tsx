824f0c5b3608845a742feb0d4edc9f6d1b85d1a9
Enrique <enrique-2001@live.com>
2024-05-31 15:24:03 -0500

    CREAR-BORRAR v1.1

--- src/app/componentes/camisas/CamiAdmin.tsx
+++ src/app/componentes/camisas/CamiAdmin.tsx
@@ -2,18 +2,16 @@ import { useState } from "react";
 import { Camisa } from "../../modelos/Camisa";
 import { ARREGLO_CAMISAS } from "../../mocks/Camisa-mocks";
 import { ARREGLO_MARCA_CAMISA } from "../../utilidades/dominios/domMarca";
-import { Button, Modal} from "react-bootstrap";
+import { Button, Modal } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
 export const CamiAdmin = () => {
   const [arrCamisas] = useState<Camisa[]>(ARREGLO_CAMISAS);
-  const[objCami, setObjCami]=useState<Camisa>(
-    new Camisa(0,"","","")
-  );
-  const [show, setShow]=useState<boolean>(false);
-  const handleClose=()=>{
+  const [objCami, setObjCami] = useState<Camisa>(new Camisa(0, "", "", ""));
+  const [show, setShow] = useState<boolean>(false);
+  const handleClose = () => {
     setShow(false);
-  }
+  };
 
   const getNombreMarca = (valor: string) => {
     for (const objMarc of ARREGLO_MARCA_CAMISA) {
@@ -22,18 +20,18 @@ export const CamiAdmin = () => {
       }
     }
   };
-  const eliminarCamisa =(codigo:number)=>{
+  const eliminarCamisa = (codigo: number) => {
     const cantidad = arrCamisas.length;
 
-    for (let i=0; i<cantidad;i++){
-      if(arrCamisas[i]!=undefined){
-        const comparar =arrCamisas[i].codCamisa
-        if(comparar==codigo){
-          arrCamisas.splice(i,1);
+    for (let i = 0; i < cantidad; i++) {
+      if (arrCamisas[i] != undefined) {
+        const comparar = arrCamisas[i].codCamisa;
+        if (comparar == codigo) {
+          arrCamisas.splice(i, 1);
         }
       }
     }
-  }
+  };
   return (
     <>
       <div className="pt-3 d-flex justify-content-center">
@@ -58,37 +56,30 @@ export const CamiAdmin = () => {
                   <td>{miCamisa.colorCamisa}</td>
                   <td className="text-center">{miCamisa.tallaCamisa}</td>
                   <td className="text-center">
-                    <a href="/#"
-                    onClick={(e)=>{
-                      e.preventDefault();
-                      setShow(true);
-                      setObjCami(miCamisa);
-                    }}>
+                    <a
+                      href="/#"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setShow(true);
+                        setObjCami(miCamisa);
+                      }}
+                    >
                       <i className="fa-solid fa-trash rojito"></i>
                     </a>{" "}
-                    <NavLink to={"/camactual/"+miCamisa.codCamisa}>
-                      <i className="fa-solid fa-pen-to-square verde" ></i>
-                      </NavLink>
-                    
-                    
+                    <NavLink to={"/camactual/" + miCamisa.codCamisa}>
+                      <i className="fa-solid fa-pen-to-square verde"></i>
+                    </NavLink>
                   </td>
                 </tr>
               ))}
             </tbody>
           </table>
-          <Modal
-            show={show}
-            onHide={handleClose}
-            backdrop="static"
-            keyboard={false}
-          >
+          <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false}>
             <Modal.Header closeButton>
               <Modal.Title>Eliminar Camisas</Modal.Title>
             </Modal.Header>
 
-            <Modal.Body>
-              ¿Está seguro de eliminar la camisa {objCami.codCamisa}?
-            </Modal.Body>
+            <Modal.Body>¿Está seguro de eliminar la camisa {objCami.codCamisa}?</Modal.Body>
 
             <Modal.Footer>
               <Button
